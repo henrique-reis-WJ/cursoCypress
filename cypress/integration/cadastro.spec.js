e69c1fd4e534507685f3bb48758a23849c59a83f
Henrique <henrique.reis@webjump.com.br>
2022-06-23 21:24:56 -0300

    Conclusão do cadastro e refatoração do código para inglês

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,6 +1,6 @@
 describe('Realizar Cadastro', () =>
 {
-    it('Criar Cadastro', () =>
+    it('Criar Cadastro com Sucesso', () =>
     {
         // ACESSAR PAGINA INICIAL //
         cy.viewport(1280, 768)
@@ -10,43 +10,107 @@ describe('Realizar Cadastro', () =>
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
-        var entregador = {
-            nome: 'Henrique Ribeiro',
+        var deliver = {
+            name: 'Henrique Ribeiro',
             cpf: '00000000011',
             email: 'henrique.reis@webjump.com.br',
             whatsapp: '11987654321',
 
-            localização: 
+            locate: 
                 {
-                    cep: '03807010',
-                    numero: '200',
-                    rua: 'Avenida José Muniz Ribeiro',
+                    postalcode: '03807010',
+                    number: '200',
+                    street: 'Avenida José Muniz Ribeiro',
                     bairro: 'Vila Paranaguá',
-                    cidade: 'São Paulo/SP'
+                    city: 'São Paulo/SP'
                 },
 
-            veiculo: 'Bicicleta',
+            deliveryMethod: 'Bicicleta',
+            cnh: '/images/cnh-digital.jpg'
+        
+        }
+        // PREENCHIMENTO DE CAMPOS //
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(deliver.locate.postalcode)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+        cy.get('input[name="address-number"]').type(deliver.locate.number)
+
+        // VERIFICAÇÃO DOS CAMPOS //
+        cy.get('input[name="address"]').should('have.value', deliver.locate.street)
+        cy.get('input[name="district"]').should('have.value', deliver.locate.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.locate.city)
+        
+        // CLICAR BOTÃO VEÍCULO PELA UTILIZANDO A LISTA //
+        cy.contains('.delivery-method li', deliver.deliveryMethod).click()
+
+        // UPLOAD DA IMAGEM //
+        cy.get('input[accept^="image"]').attachFile(deliver.cnh)
+        cy.get('button[class="button-success"]').click()
+
+        // MENSAGEM DE VERIFICAÇÃO //
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+    })
+
+    it('Cadastro CPF Inválido', () =>
+    {
+        // ACESSAR PAGINA INICIAL //
+        cy.viewport(1280, 768)
+        cy.visit('https://buger-eats.vercel.app')
+
+        // CLICAR BOTÃO DE CADASTRO // 
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var deliver = {
+            name: 'Henrique Ribeiro',
+            cpf: '000000000kk',
+            email: 'henrique.reis@webjump.com.br',
+            whatsapp: '11987654321',
+
+            locate: 
+                {
+                    postalcode: '03807010',
+                    number: '200',
+                    street: 'Avenida José Muniz Ribeiro',
+                    bairro: 'Vila Paranaguá',
+                    city: 'São Paulo/SP'
+                },
+
+            deliveryMethod: 'Bicicleta',
             cnh: '/images/cnh-digital.jpg'
         
         }
 
         // PREENCHIMENTO DE CAMPOS //
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf)
-        cy.get('input[name="email"]').type(entregador.email)
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
 
-        cy.get('input[name="postalcode"]').type(entregador.localização.cep)
+        cy.get('input[name="postalcode"]').type(deliver.locate.postalcode)
         cy.get('input[type=button][value="Buscar CEP"]').click()
-        cy.get('input[name="address-number"]').type(entregador.localização.numero)
+        cy.get('input[name="address-number"]').type(deliver.locate.number)
 
         // VERIFICAÇÃO DOS CAMPOS //
-        cy.get('input[name="address"]').should('have.value', entregador.localização.rua)
-        cy.get('input[name="district"]').should('have.value', entregador.localização.bairro)
-        cy.get('input[name="city-uf"]').should('have.value', entregador.localização.cidade)
+        cy.get('input[name="address"]').should('have.value', deliver.locate.street)
+        cy.get('input[name="district"]').should('have.value', deliver.locate.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.locate.city)
         
-        cy.contains('.delivery-method li', entregador.veiculo).click()
+        // CLICAR BOTÃO VEÍCULO PELA UTILIZANDO A LISTA //
+        cy.contains('.delivery-method li', deliver.deliveryMethod).click()
 
-        cy.get('input[accept^="image"]').attachFile(entregador.cnh)
+        // UPLOAD DA IMAGEM //
+        cy.get('input[accept^="image"]').attachFile(deliver.cnh)
+
+        // CONCLUIR CADASTRO //
+        cy.get('form button[type="submit"]').click()
+
+        const errorMessage = 'Oops! CPF inválido'
+        cy.get('.alert-error').should('have.text', errorMessage)
     })
 })
